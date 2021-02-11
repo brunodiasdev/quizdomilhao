25bbdd793b4c64fd202c0e5dfcf26bc6c2d4b236
brunodiasdev <brunodiasdevs@gmail.com>
2021-02-11 11:46:16 -0300

    New

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 /*Crianto array de perguntas*/
 
 const perguntas = [
-    p1 = {
-        pergunta = 'Qual sobrenome no Mickey, desenho da Disney?',
+    p1 = [
+        pergunta = "Qual sobrenome no Mickey, desenho da Disney?",
         respostas = [ 'Donald', 'Julio', 'venâncio', 'Mouse']
-    },
-    p2 = {
+    ],
+    p2 = [
         pergunta = 'Qual a capital do Brasil?',
         respostas = [ 'Prado', 'Japão', 'São Paulo', 'Brasília']
-    },
-    p3 = {
+    ],
+    p3 = [
         pergunta = 'Em que país fica o Hawaii',
         respostas = [ 'Brasil', 'Congo', 'India', 'USA']
-    }
+    ]
 ]
 
-console.log(perguntas)
+function inicio(obj){
+    var n = document.getElementsByClassName(respostaA)
+    n = obj.innerHTML = p1[0]
+    return n
+}
+
+console.log(p2[1][3])
