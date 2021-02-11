847d65d2f091dbc22489b4603c40ec727138fbc1
brunodiasdev <brunodiasdevs@gmail.com>
2021-02-11 10:45:56 -0300

    Ajustes CSS Botão Iniciar

---
+++ index.js
@@ -0,0 +1,18 @@
+/*Crianto array de perguntas*/
+
+const perguntas = [
+    p1 = {
+        pergunta = 'Qual sobrenome no Mickey, desenho da Disney?',
+        respostas = [ 'Donald', 'Julio', 'venâncio', 'Mouse']
+    },
+    p2 = {
+        pergunta = 'Qual a capital do Brasil?',
+        respostas = [ 'Prado', 'Japão', 'São Paulo', 'Brasília']
+    },
+    p3 = {
+        pergunta = 'Em que país fica o Hawaii',
+        respostas = [ 'Brasil', 'Congo', 'India', 'USA']
+    }
+]
+
+console.log(perguntas)
