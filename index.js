511ea5d023681a379acbd9e50f2ec8f974f50b2d
brunodiasdev <brunodiasdevs@gmail.com>
2021-02-18 18:23:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 /*Crianto array de perguntas*/
 let iniproj = document.querySelector('.comecar')
 let perg = document.querySelector('.pergunta')
+let respA = document.querySelector('.respostaA')
+let respB = document.querySelector('.respostaB')
+let respC = document.querySelector('.respostaC')
+let respD = document.querySelector('.respostaD')
 
 iniproj.onclick = function(){
     ini()
@@ -22,13 +26,27 @@ const perguntas = [
 function ini(){
     pergnov = perguntas[num][0]
     perg.innerHTML = pergnov
+    respA.innerHTML = perguntas[num][1][numVar]
+    respB.innerHTML = perguntas[num][1][numVar]
+    respC.innerHTML = perguntas[num][1][numVar]
+    respD.innerHTML = perguntas[num][1][numVar]
 }
+
 var num = parseInt(variante())
+
 function variante() {
    let x = Math.random() * (perguntas.length - 0) + 0;
    let num = x
    return num   
 }
+
+var numVar = parseInt(varianteResp())
+
+function varianteResp(){
+    let x = Math.random() * (3 - 0) + 0;
+    return x
+}
+
 console.log(num)
 function novaPergunta(){
     let i = 0
